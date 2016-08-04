25711e390cba587685a5ac476b7b974c7260e0dd
Andy Fang <andy@mingtak.com.tw>
2016-08-04 21:03:44 +0800

    bugfix, and using chinese, importCPC.py

--- src/twNotice/content/browser/importCPC.py
+++ src/twNotice/content/browser/importCPC.py
@@ -10,11 +10,13 @@ from z3c.relationfield.relation import RelationValue
 from zope import component
 from zope.app.intid.interfaces import IIntIds
 from zope.event import notify
-from zope.lifecycleevent import ObjectCreatedEvent
+from zope.lifecycleevent import ObjectCreatedEvent, ObjectModifiedEvent
 from DateTime import DateTime
 import transaction
 import logging
 from Products.CMFPlone.utils import safe_unicode
+from plone.protect.interfaces import IDisableCSRFProtection
+from zope.interface import alsoProvides
 
 
 logger = logging.getLogger("twNotice.content.import_cpc")
@@ -31,27 +33,25 @@ class ImportCPC(BrowserView):
         portal = api.portal.get()
         intIds = component.getUtility(IIntIds)
 
+        alsoProvides(request, IDisableCSRFProtection)
 
         with open('/home/playgroup/CpcOpenData.xml') as file:
             soup = BeautifulSoup(file.read(), 'xml')
 
-        for item in soup.find_all('object'):
-#            import pdb;pdb.set_trace()
+        for item in soup.find_all(safe_unicode('採購標的')):
             cpc = api.content.create(
                 type='CPC',
-                id=unicode(item.code.string),
-                title=unicode(item.cname.string),
-                engTitle=unicode(item.ename.string),
-                noticeCategory=unicode(item.category.string),
+                id=safe_unicode(getattr(item, '標的代碼').string.encode('utf-8')),
+                title=safe_unicode(getattr(item, '分類名稱中文').string.encode('utf-8')),
+                engTitle=safe_unicode(getattr(item, '分類名稱英文').string.encode('utf-8')),
+                noticeCategory=safe_unicode(getattr(item, '標的分類').string.encode('utf-8')),
                 container=portal['resource']['category']
             )
             cpc.childrenCPC = {cpc.id:cpc.title}
-            if item.cpcsub.string:
-                for cpcsub in item.cpcsub.string.split(','):
-                    key = cpcsub.split(' ')[0]
-                    value = cpcsub.split(' ')[1]
+            if getattr(item, '本分類之下1層CPC細項').string:
+                for cpcsub in getattr(item, '本分類之下1層CPC細項').string.split(','):
+                    key = cpcsub.split()[0]
+                    value = cpcsub.split()[1]
                     cpc.childrenCPC[key] = value
-            cpc.reindexObject('childrenCPC')
+            notify(ObjectModifiedEvent(cpc))
             logger.info(cpc.title)
-
-        transaction.commit()
